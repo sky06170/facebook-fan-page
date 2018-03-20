cf46aa610e1a0da75457a42bbe40c1db23d87a5f
Rick <sky06170@hotmail.com>
2018-03-20 18:12:03 +0800

    Edit dir

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,14 +1,14 @@
 <?php
 
 function facebookFanPageOptions()
-{
+{	
 	return [
 
 		'version' => 'v2.12', //your facebook graph api version
 
-		'pageID' => '', //your facebook fan page ID
+		'pageID' => 1261910873819373, //your facebook fan page ID
 
-		'pageToken' => '', //your facebook fan page Token
+		'pageToken' => 'EAACFd7AeCzMBAFdBh3d5zKNcHrvHEji9dVgoB7ZADIMbkmwZCnFV3TuZBslOSA06XcTXA4ZA3XOtkwjJoMWjLZBZAiazXFUocBtvMyxXbolWpzFJZA9SpJdHQIvjZACDYnVRNYXqAgLNzSWYrCLxbDqMyHP2LSIyvwBlZBgK8XAwos0YfWE3WzQCB', //your facebook fan page Token
 
 		'utc' => 8 //Universal Time Coordinated
 
